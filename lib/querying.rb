e4a9e1815272ca3aa5f2ceef5c920109c9c99e5e
zhifeng ou <zhifengou@hotmail.com>
2018-09-24 22:32:16 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,25 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books WHERE books.series_id=1 GROUP BY books.year ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name,characters.motto FROM characters WHERE length(characters.motto) =
+(select max(length(characters.motto)) FROM characters);"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT MAX(characters.species),COUNT(characters.species) FROM characters GROUP BY characters.species ORDER BY characters.species DESC limit 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name,subgenres.name FROM authors JOIN subgenres ON authors.id=subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series JOIN characters ON characters.series_id=series.id GROUP BY series.title ORDER BY series.title limit 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name,COUNT(character_books.character_id) FROM characters JOIN character_books WHERE characters.id=character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.character_id) DESC;"
 end
---
+++ lib/update.sql
@@ -0,0 +1,1 @@
+UPDATE characters set species="Martian" WHERE id=8;
